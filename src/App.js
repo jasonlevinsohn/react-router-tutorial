76376b60177232473319c67d6733df74bd9aa180
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-04-11 17:28:31 -0400

    finished lesson 9, Querystring Parameters

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import {Router, Route, Link, hashHistory, IndexRoute} from 'react-router';
 
-const Message = (props) =>
-    <div><h1>{props.params.message || 'Hello Dare'}</h1><Links /></div>
-
-const Home = () => <h1>Home</h1>
-const HomeBody = () => <div>this is the home body</div>
-const Other = () => <h1>Other</h1>
-const OtherBody = () => <div>this is the other body</div>
-
-const Container = (props) =>
-    <div>{props.header}{props.body}<Links /></div>
+const Page = (props) => <div><h1>{props.location.query.message || 'Hello dare again'}</h1><Links /></div>
 
 const Links = () =>
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/other">Other</Link>
+        <Link to={ {pathname: '/', query: {message: 'Yo'}} }>Yo</Link>
+        <Link to={ {pathname: '/', query: {message: 'Dog'}} }>Dog</Link>
+        <Link to={ {pathname: '/', query: {message: 'Peace'}} }>Peace</Link>
     </nav>
 
 
@@ -23,10 +15,7 @@ class App extends React.Component {
     render() {
         return (
             <Router history={ hashHistory }>
-                <Route path="/" component={Container}>
-                    <IndexRoute components={{header: Home, body: HomeBody}}></IndexRoute>
-                    <Route path="/other" components={{header: Other, body: OtherBody}}></Route>
-                </Route>
+                <Route path="/" component={Page}></Route>
             </Router>
         );
     }
