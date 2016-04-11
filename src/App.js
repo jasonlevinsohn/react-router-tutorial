baa5ab3d1b9603092ae7a6c762c174e4ddd1f897
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-04-11 17:04:23 -0400

    finished lesson 7, Route Parameters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import {Router, Route, Link, hashHistory, IndexRoute} from 'react-router';
 
-const Outer = (props) => <div><h1>Our Site</h1><Links />{props.children}</div>;
-const About = () => <div><h1>About</h1></div>;
-const Contact = () => <div><h1>Contact</h1></div>;
+const Message = (props) =>
+    <div><h1>{props.params.message || 'Hello Dare'}</h1><Links /></div>
 
 const Links = () =>
-    <nav>
-        <Link activeStyle={{color: 'green'}} to="/">Home</Link>
-        <Link activeStyle={{color: 'green'}} to="/about">About</Link>
-        <Link activeClassName="active" to="/contact">Contact</Link>
-    </nav>
+   <nav>
+    <Link to="/">Hello</Link>
+    <Link to="/Hi">Hi</Link>
+    <Link to="/Yo">Yo Low</Link>
+   
+   </nav>
 
+// Parenths around message in Route means that its optional
 class App extends React.Component {
     render() {
         return (
             <Router history={ hashHistory }>
-                <Route path="/" component={Outer}>
-                    <IndexRoute component={About}></IndexRoute>
-                    <Route path="contact" component={Contact}></Route>
-                </Route>
+                <Route path="/(:message)" component={Message}></Route>
             </Router>
         );
     }
